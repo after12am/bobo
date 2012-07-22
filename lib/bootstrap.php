4da846e42376ddf4556db12e20a79c3d781f3b95
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-07-22 11:22:05 +0900

    change path to absolute path

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -6,6 +6,6 @@ mb_internal_encoding('UTF-8');
 
 // set include path for batch processing.
 // it is better not to set using htaccess.
-set_include_path(get_include_path() . PATH_SEPARATOR . realpath('../db'));
-set_include_path(get_include_path() . PATH_SEPARATOR . realpath('../lib'));
-set_include_path(get_include_path() . PATH_SEPARATOR . realpath('../lib/contrib'));
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__) . '/../db'));
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__) . '/../lib'));
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__) . '/../lib/contrib'));
