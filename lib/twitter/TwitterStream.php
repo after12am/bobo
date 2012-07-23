d3d6d3515e9e3566cd87cda1c3b5ec1159f2d2c9
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-07-24 08:05:21 +0900

    change internal design

--- lib/twitter/TwitterStream.php
+++ lib/twitter/TwitterStream.php
@@ -5,6 +5,10 @@ class TwitterStream extends TwitterAPI {
     
     protected $userid;
     protected $passwd;
+    protected $host = "stream.twitter.com";
+    protected $path = "/1/statuses/sample.json";
+    protected $port = 443;
+    protected $timeout = 30;
     protected $fp;
     protected $errno;
     protected $errmsg;
@@ -13,6 +17,7 @@ class TwitterStream extends TwitterAPI {
         
         $this->userid = $userid;
         $this->passwd = $passwd;
+        
         parent::__construct($consumer_key, $consumer_secret, $access_token, $access_token_secret);
     }
     
@@ -23,7 +28,7 @@ class TwitterStream extends TwitterAPI {
         }
     }
     
-    protected function open($host = "stream.twitter.com", $path = "/1/statuses/sample.json", $port = 443, $timeout = 30) {
+    protected function open() {
         
         $this->errno = 0;
         $this->errmsg = "";
@@ -31,24 +36,24 @@ class TwitterStream extends TwitterAPI {
         // try to establish a connection to a streaming
         // for being delivered a feed of Tweets, without
         // needing to worry about polling or REST API rate limits.
-        $this->fp = fsockopen("ssl://{$host}", $port, $this->errno, $this->errmsg, $timeout);
+        $this->fp = fsockopen("ssl://{$this->host}", $this->port, $this->errno, $this->errmsg, $this->timeout);
         
         if ($this->fp === false) {
              return false;
         }
         
-        $this->connect($host, $path);
+        $this->connect();
         
         return $this->fp;
     }
     
-    private function connect($host, $path) {
+    private function connect() {
         
         $ver = phpversion();
         $basic = base64_encode("{$this->userid}:{$this->passwd}");
         
-        $req  = "GET {$path} HTTP/1.1\r\n";
-        $req .= "Host: {$host}\r\n";
+        $req  = "GET {$this->path} HTTP/1.1\r\n";
+        $req .= "Host: {$this->host}\r\n";
         $req .= "User-Agent: PHP/{$ver}\r\n";
         $req .= "Authorization: Basic {$basic}\r\n";
         $req .= "Connection: Close\r\n\r\n";
