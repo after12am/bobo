66bc1595c4f6ab2b6a591b237d47266c0af0abc6
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-07-24 07:53:40 +0900

    set timezone

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -4,6 +4,9 @@ require_once('constants.php');
 // set internal encoding with utf-8.
 mb_internal_encoding('UTF-8');
 
+// set timezone
+date_default_timezone_set('Asia/Tokyo');
+
 // set include path for batch processing.
 // it is better not to set using htaccess.
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__) . '/../lib'));
