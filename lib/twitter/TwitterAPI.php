7360f204a2d95fd52888a0a338ea805f6c38f21c
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-07-26 08:48:36 +0900

    change internal design

--- lib/twitter/TwitterAPI.php
+++ lib/twitter/TwitterAPI.php
@@ -15,6 +15,7 @@ class TwitterAPI {
         $this->consumer_secret = $consumer_secret;
         $this->access_token = $access_token;
         $this->access_token_secret = $access_token_secret;
+        
         $this->twitter = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
     }
 }
