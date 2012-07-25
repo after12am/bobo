4426b43388b527bee90dd47e093dc8c12864751b
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-07-26 08:47:50 +0900

    change api name

--- lib/yahoo/MAService.php
+++ lib/yahoo/MAService.php
@@ -3,7 +3,7 @@ require_once('YahooAPI.php');
 
 class MAService extends YahooAPI {
     
-    const MAService_API = 'http://jlp.yahooapis.jp/MAService/V1/parse';
+    const API = 'http://jlp.yahooapis.jp/MAService/V1/parse';
     
     public function __construct($appid) {
         
@@ -12,7 +12,7 @@ class MAService extends YahooAPI {
     
     public function analyse($sentence, $results = 'ma') {
         
-        $base = self::MAService_API;
+        $base = self::API;
         $appid = $this->appid;
         $sentence = urlencode($sentence);
         $api = "{$base}?appid={$appid}&results={$results}&sentence={$sentence}";
