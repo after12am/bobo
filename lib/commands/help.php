c7496108c4d124661580c8870e7d420ae9efa1c5
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-11-28 20:19:48 +0900

    update help

--- lib/commands/help.php
+++ lib/commands/help.php
@@ -3,7 +3,7 @@ echo "\n  usage : php bot.php [option]\n";
 echo "\n  the below are available options.\n";
 echo "\n";
 echo "  setup   setup database.\n";
-echo "  post    tweet using a second-order markov chain.\n";
 echo "  pick    pick up recent tweets was filtered by language with jp.\n";
+echo "  tweet   tweet using a second-order markov chain.\n";
 echo "  help    show help.\n";
 echo "\n";
